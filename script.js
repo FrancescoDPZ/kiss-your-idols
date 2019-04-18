34a779680d581c1b6994ac8d8945fd48d4541544
FrancescoDPZ <38539670+FrancescoDPZ@users.noreply.github.com>
2019-04-18 17:04:59 -0400

    update js
    toggle replaced with hide/show functions

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 
 
 
+/*-----------------------------------------
+
+PAGE ANIMATION AND INTERACTION
+
+------------------------------------------------------*/
+
+
+
+
 
 var myFunction1 = function () {
     var img = document.querySelector('img');
@@ -9,53 +18,53 @@ var myFunction1 = function () {
     var newSrc = '';
     if (img_name === 'trump.jpg') {
         newSrc = "reactions/trump-sad.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
 
     } else if (img_name === 'obama.jpg') {
         var newSrc = "reactions/obama-sad.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     
     } else if (img_name === 'jennifer_aniston.jpg') {
         var newSrc = "reactions/aniston-punch.gif";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     
     } else if (img_name === 'bill_murray.jpg') {
         var newSrc = "reactions/bill_murray-punch.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     
     } else if (img_name === 'emma_stone.jpg') {
         var newSrc = "reactions/emma-punch.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     
     } else if (img_name === 'oprah.jpg') {
         var newSrc = "reactions/oprah-punch.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     
     } else if (img_name === 'drake.jpg') {
         var newSrc = "reactions/drake-punch.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     
     } else if (img_name === 'jon_snow.jpg') {
         var newSrc = "reactions/jon_snow-punch.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     
     } else if (img_name === 'britney.jpg') {
         var newSrc = "reactions/britney-punch.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     
     } else if (img_name === 'snoopy.png') {
         var newSrc = "reactions/snoopy-punch.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+            jQuery('.buttons-box').hide();      
+            jQuery('#next').show();
     }
     img.src = newSrc;
 };
@@ -72,53 +81,53 @@ var myFunction2 = function () {
     var newSrc = '';
     if (img_name === 'trump.jpg') {
         newSrc = "reactions/trump-laugh.jpg";
-        jQuery('.buttons-box').toggle('hide');      
-        jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
 
     } else if (img_name === 'obama.jpg') {
         var newSrc = "reactions/obama-laugh.jpg";
-        jQuery('.buttons-box').toggle('hide');      
-        jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
 
     } else if (img_name === 'jennifer_aniston.jpg') {
         var newSrc = "reactions/aniston-laugh.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'bill_murray.jpg') {
         var newSrc = "reactions/bill_murray-laugh.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'emma_stone.jpg') {
         var newSrc = "reactions/emma-laugh.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'oprah.jpg') {
         var newSrc = "reactions/oprah-laugh.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'drake.jpg') {
         var newSrc = "reactions/drake-laugh.png";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'jon_snow.jpg') {
         var newSrc = "reactions/jon_snow-laugh.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'britney.jpg') {
         var newSrc = "reactions/britney-laugh.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'snoopy.png') {
         var newSrc = "reactions/snoopy-laugh.gif";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     }
     img.src = newSrc;
 };
@@ -135,53 +144,53 @@ var myFunction3 = function () {
     var newSrc = '';
     if (img_name === 'trump.jpg') {
         newSrc = "reactions/trump-kiss.jpg";
-        jQuery('.buttons-box').toggle('hide');      
-        jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
 
     } else if (img_name === 'obama.jpg') {
         var newSrc = "reactions/obama-kiss.jpg";
-        jQuery('.buttons-box').toggle('hide');      
-        jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
 
     } else if (img_name === 'jennifer_aniston.jpg') {
         var newSrc = "reactions/aniston-kiss.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'bill_murray.jpg') {
         var newSrc = "reactions/bill_murray-kiss.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'emma_stone.jpg') {
         var newSrc = "reactions/emma-kiss.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'oprah.jpg') {
         var newSrc = "reactions/oprah-kiss.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'drake.jpg') {
         var newSrc = "reactions/drake-kiss.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'jon_snow.jpg') {
         var newSrc = "reactions/jon_snow-kiss.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'britney.jpg') {
         var newSrc = "reactions/britney-kiss.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     
     } else if (img_name === 'snoopy.png') {
         var newSrc = "reactions/snoopy-kiss.jpg";
-            jQuery('.buttons-box').toggle('hide');      
-            jQuery('#next').toggle('show');
+        jQuery('.buttons-box').hide();      
+        jQuery('#next').show();
     }
     img.src = newSrc;
 };
@@ -192,3 +201,8 @@ function refreshPage(){
     window.location.reload();
 } 
 
+
+/*-----------------------------------------------------
+
+
+-------------------------------------------------------------------------------*/
